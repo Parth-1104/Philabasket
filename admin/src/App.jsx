53c570b5671a83b8fb4a75ed01a024a9f48d4bad
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2026-03-18 18:01:29 +0530

    added impersonate user

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -28,7 +28,8 @@ import OrderDetail from './pages/OrderDetail'
 import UserDetail from './components/UserDetails'
 import ContactMessages from './pages/ContactUs'
 import TriviaManager from './pages/TriviaManager'
-import TriviaLeaderboard from './pages/TriviaLeaderboard.jsx'
+import TriviaLeaderboard from './pages/TriviaLeaderBoard'
+
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
 export const frontendUrl = import.meta.env.VITE_FRONTEND_URL;
 export const currency = '$'
